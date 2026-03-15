e18e491633500cb1b946625aebbbf3c8cb785582
spacotto <silvia.pacotto@gmail.com>
2026-03-15 01:19:11 +0100

    Update elements.py

--- alchemy/elements.py
+++ alchemy/elements.py
@@ -1,1 +1,21 @@
+"""
+Create your first alchemical package
+and learn how __init__.py controls
+what magic is available to other alchemists.
+"""
 
+
+def create_fire() -> str:
+    return "Fire element created"
+
+
+def create_water() -> str:
+    return "Water element created"
+
+
+def create_earth() -> str:
+    return "Earth element created"
+ 
+ 
+def create_air() -> str:
+    return "Air element created"
